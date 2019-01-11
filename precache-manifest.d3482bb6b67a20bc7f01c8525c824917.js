0bc4d5e4c93d0b2af3fc45916e0a57dec7ed7d6a
Marcin Piekarniak <marcin@radcode.co>
2019-01-11 09:08:29 +0100

    Updates

---
+++ precache-manifest.d3482bb6b67a20bc7f01c8525c824917.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b2c826189e7851e7fcd",
+    "url": "/mapka4/static/css/main.82794574.chunk.css"
+  },
+  {
+    "revision": "3b2c826189e7851e7fcd",
+    "url": "/mapka4/static/js/main.3b2c8261.chunk.js"
+  },
+  {
+    "revision": "625f8b392398bef2a69b",
+    "url": "/mapka4/static/js/1.625f8b39.chunk.js"
+  },
+  {
+    "revision": "5256df58950961baf0b6",
+    "url": "/mapka4/static/js/runtime~main.5256df58.js"
+  },
+  {
+    "revision": "6764f789622ff0aac7034723137f17c4",
+    "url": "/mapka4/index.html"
+  }
+];
